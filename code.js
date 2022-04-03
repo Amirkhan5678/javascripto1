47cc69aecab5c758686d55b6d72c96c729b4296f
Amirkhan5678 <sh4444qtr@gmail.com>
2022-04-04 04:58:00 +0500

    js assignment is done

---
+++ code.js
@@ -0,0 +1,343 @@
+/// CHAPTER 1 ///
+
+/// Question no: 1///
+
+// var string=("error! please enter a valiid password")
+// alert( string );
+  
+/// Question no: 2///
+
+// var stringOne=("welcome to js land...  happy coding ")
+// alert( stringOne );
+
+/// Question no: 3///
+
+// var stringOne=("welcome to js land...");
+// alert(stringOne);
+// var stringTwo=("happy coding !");
+// alert(stringTwo);
+// var stringThree=("hello... i can run js through my web browser's console");
+// alert( stringThree);
+ 
+/// Chapter no: 2
+/// Question no: 1///
+
+// var userName=("amir khan");
+// alert(userName);
+// var age=(21);
+// alert(age)
+// var message=("certified mob & web development");
+// alert(message)
+
+/// Question no: 2///
+
+// var sentence=("pizza");
+// alert(sentence);
+
+
+/// Question no: 3///
+
+// var email=("my email address is example@example.com");
+// alert(email);
+
+/// Question no: 4///
+
+// var book=("i am trying to learn from the book A samrter way to learn javascipt");
+// alert(book);
+
+  /// Question no: 5 ///
+
+//   var message=("Yah! i can write HTML content through js ")
+//   alert(message);
+  
+/// Chapter no: 03 ///
+ /// Question no: 1 ///
+  
+//  var age=(15);
+//  alert(age);
+
+/// Question no: 2 ///
+  
+// var tracker=("you have visited this website 10 times");
+// alert(tracker);
+
+/// Question no: 3 ///
+
+// var birthYear=("<b>my birth year is 2001</b>");
+// document.write(birthYear);
+
+/// Question no: 4 ///
+
+// var name=("john orderd");
+// document.write(name);
+// var qty=( 15 );
+// document.write(qty);
+// var product=("T-shirt from XYZ company");
+// document.write(product);
+
+/// Chapter no: 04 ///
+/// Question no: 1 ///
+
+// var heading=("<h1><b>Rules for naming JS variables</b></h1>");
+// document.write(heading);
+// var values=("variable name can only contain__numbers,null,undefined________");
+// document.write(values);
+// var sentence=("</br> varaible case__ camel case , snake case");
+// document.write(sentence);
+// var string=("</br> variable which cannot in JS___upper case, space between variables");
+// document.write(string);
+
+/// Chapter no: 05 ///
+/// Question no: 1 ///
+
+// var num1=(3);
+// var num2=(5);
+// var result=(3+5)
+//  document.write("sum of 3 and 5 is", + result);
+
+/// Question no: 2 ///
+
+// var num1=(2);
+// var num2=(4);
+// var add=(2+4);
+// document.write(" value after addition is : ", add)
+// var sub=(4-2);
+// document.write("<br/>value after subtraction is : ",sub);
+// var product=(2*4);
+// document.write("<br/>value after multiplication is : ",product);
+// var div=(4/2);
+// document.write("<br/>value after division is : ",div);
+// var modulus=(4%2);
+// document.write("<br/>value of remainder is : ",modulus);
+
+/// Question no: 3 ///
+
+// var num1=(600);
+// var product=(600*5);
+// document.write("total cost to buy 5 ticket is : ",product);
+
+/// Question no: 4 ///
+
+// var num1=(4);
+// var num2=(1);
+// var num3=(2);
+// var num4=(3);
+// var num5=(4);
+// var num6=(5);
+// var num7=(6);
+// var num8=(7);
+// var num9=(8);
+// var num10=(9);
+// var num11=(10);
+// document.write("<h2>Table </h2>")
+// document.write("4 x 1 = ",num1*num2);
+// document.write("<br /> 4 x 2 = ",num1*num3);
+// document.write("<br /> 4 x 3 = ",num1*num4);
+// document.write("<br /> 4 x 4 = ",num1*num5);
+// document.write("<br /> 4 x 5 = ",num1*num6);
+// document.write("<br /> 4 x 6 = ",num1*num7);
+// document.write("<br /> 4 x 7 = ",num1*num8);
+// document.write("<br /> 4 x 8 = ",num1*num9);
+// document.write("<br /> 4 x 9 = ",num1*num10);
+// document.write("<br /> 4 x 10 = ",num1*num11);
+
+/// Question no: 5 ///
+
+// var value=(50);
+// var result=((50-32)*5/9);
+// document.write("50 C is "+ result + "F");
+
+// var result=((50*9/5) +32);
+// document.write("50 F is" + result + " C ");
+
+/// Question no: 6 ///
+
+// var item1=(300);
+// var item2=(200);
+// var qty1=(5);
+// var qty2=(2);
+//   document.write("Qty of T- shirt is 5 <br />");
+//   document.write("Qty of pents is 2 <br />");
+//   document.write("Cost of T- shirt is 300 <br />");
+//   document.write("Cost of pents is 200 ");
+
+// document.write("Total cost of item1 is" + item1*qty1 + "<br />" + "Total cost of item2 is " +item2*qty2);
+
+/// Question no: 7 ///
+
+// var Total=(500);
+// var obtained=(300);
+// var result=(obtained*100/Total);
+// document.write("Total marks is 500<br />");
+// document.write("Obtained marks is 300 <br />");
+// document.write("percentage is " + result);
+
+/// Question no: 8 ///
+
+// var america=(1);
+// var pak=(150);
+// var result=(america*pak)
+
+// document.write("America 1 dollar is 150 <br />");
+// document.write("Pakistan 1 rupees 1 " + pak + "<br />");
+// document.write( "Total currency is" + result + "<br />");
+
+/// Question no: 9 ///
+
+// var current=(2022)
+// var birth=(2001)
+//   var result=(current-birth)
+//   document.write("Current year is " +current+ "<br />");
+//   document.write("Birth year is " +birth + "<br />");
+//   document.write("Your Age is " + resuslt);
+
+/// Question no: 10 ///
+
+// var radius=(20);
+// var result=( 2*3.142*radius)
+// var area=(radius*2*3.142)
+//  document.write( "Radius of a circle is " + 20 +  "<br />");
+//  document.write("Circumference is " + radius + "<br />");
+//  document.write("Area is " +area)
+ 
+///// CHAPTER NO 06 /////
+
+///// QUESTION NO 01 ////
+
+// var value=(10);
+// document.write(" value of a is " + ++value + "<br />");
+// document.write(" value of a is " + value++);
+// document.write(" value of a is " + --value);
+
+ ///// QUESTION NO 02 ////
+ 
+//  var a=(2);
+//  var b=(1);
+
+//  document.write(--a +"<br />")
+//  document.write(--a - --b + "<br />");
+//  document.write(--a - --b + ++b + "<br />");
+//  document.write(--a - --b + ++b + b--);
+
+ ///// QUESTION NO 03 ////
+
+//  var a=prompt("Enter your name");
+//  var b=("welcome to my website");
+//  alert(a);
+//  alert(b);
+ 
+ ///// QUESTION NO 04 ////
+
+//  var a=+prompt("Enter any number");
+//  alert(a);
+// var result=(a*1)
+//  if(a){
+//    document.write(a+ " x "+" 1 " + " = " + result)
+//  else{
+//      document.write(5 +" x " + " 1 " + " = " + 5*1 + "<br />")
+//      document.write(5 +" x " + " 2 " + " = " + 5*2 + "<br />")
+//      document.write(5 +" x " + " 3 " + " = " + 5*3 + "<br />")  
+//      document.write(5 +" x " + " 4 " + " = " + 5*4 + "<br />")
+//      document.write(5 +" x " + " 5 " + " = " + 5*5 + "<br />")
+//      document.write(5 +" x " + " 6 " + " = " + 5*6 + "<br />")
+//      document.write(5 +" x " + " 7 " + " = " + 5*7 + "<br />")
+//      document.write(5 +" x " + " 8 " + " = " + 5*8 + "<br />")
+//      document.write(5 +" x " + " 9 " + " = " + 5*9 + "<br />")
+//      document.write(5 +" x " + " 10 " + " = " + 5*10 )
+
+//  }
+//   };
+ 
+ ///// QUESTION NO 05 ////
+
+//  var Total=(100);
+//  var english=(54);
+//  var math=(54);
+//  var science=(48);
+//  var result1=(54*100/100);
+//  var result2=(48*100/100);
+// document.write("<b>Subject</b>" +"<b> Total </b>" + "<b>Obtained</b>"+"<br />")
+//  document.write(" Engish "+ "100" + " 54 " + result1 +"%"+ "<br />");
+//  document.write(" Math  "  + "100" + "  54 " + result1 + "%"+"<br />");
+//  document.write(" Science "+ "100" + " 54 " + result1+"%");
+
+//// Chapter no 09 /////
+
+ ///// QUESTION NO 01 ////
+
+//  var a=prompt("Enter your city Name");
+ 
+//  if(a=="karachi"){
+//        alert("Welcome to city of lights")
+
+//  }
+
+ ///// QUESTION NO 02 ////
+
+//  var a=prompt("Enter your gender")
+
+//  if(a=="male"){
+//    alert("Good morning sir")
+//  }
+//  else{ alert("Good morning ma'm ")}
+
+ ///// QUESTION NO 03 ////
+
+//  var a=prompt("Enter  traffic color");
+
+//  if(a=="red"){
+//    alert("must stop")
+//  }
+//  else{ alert("ready to move");
+
+//  }
+
+ ///// QUESTION NO 04 ////
+
+//  var a=prompt(" Enter your remaining fuel ");
+
+//  if(a<0.25){ 
+//    alert("please refill your fuel tank");
+
+//  }
+//  else{
+//    alert("Enjoy your ride");
+//  }
+
+ ///// QUESTION NO 05 ////
+
+//  var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+//  var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+//  if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+ ///// QUESTION NO 06 ////
